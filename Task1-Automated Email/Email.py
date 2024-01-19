c28b8b5f9785111c889988b0633fe602525d6a50
ShreyansuPanda <152596239+ShreyansuPanda@users.noreply.github.com>
2024-01-19 19:38:33 +0530

    Email.py

--- Task1-Automated Email/Email.py
+++ Task1-Automated Email/Email.py
@@ -2,15 +2,25 @@
 from email.message import EmailMessage
 import ssl
 import smtplib
+import pandas as pd
 
-email_sender= #Add the sender's email
-email_password = #Add app password from gmail
-email_receiver=#Add receiver's email
+email_sender="eziomorningstar123@gmail.com"
+password=r"E:\programming\Python\CodesonBytes\Task 1-Automated Email\PASSWORD.txt"
+
+#For the email of receivers we are importing the data from an Excel file, Update the path of Excel as required
+excel_file_path=r"E:\programming\Python\CodesonBytes\Task 1-Automated Email\Email Receivers.xlsx"
+
 
 subject="CodesonBytes Task 1:Automated Email"
 body="""
-Test email sent to show that the code is working!
+Hello, This is a Test email sent to show that the code for automated email is working!
 """
+
+with open(password, 'r') as password:
+        email_password = password.read().strip()
+
+df = pd.read_excel(excel_file_path)
+email_receiver= df['Email'].tolist()
 
 em=EmailMessage()
 em['From']=email_sender
